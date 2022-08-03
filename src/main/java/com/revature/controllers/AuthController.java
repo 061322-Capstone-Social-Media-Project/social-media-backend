d742c76ba1190d438d4dea4bd8cf98699012a0c1
EFerri222 <eferri222@gmail.com>
2022-08-03 16:40:40 -0400

    Update user model to match our front end, update data.sql to match user model

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -42,14 +42,14 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody RegisterRequest registerRequest) {
-        User created = new User(0,
-                registerRequest.getEmail(),
-                registerRequest.getPassword(),
-                registerRequest.getFirstName(),
-                registerRequest.getLastName());
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(created));
-    }
+//    @PostMapping("/register")
+//    public ResponseEntity<User> register(@RequestBody RegisterRequest registerRequest) {
+//        User created = new User(0,
+//                registerRequest.getEmail(),
+//                registerRequest.getPassword(),
+//                registerRequest.getFirstName(),
+//                registerRequest.getLastName());
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(created));
+//    }
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -16,8 +16,22 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name="email")
     private String email;
+    @Column(name="password")
     private String password;
+    @Column(name="first_name")
     private String firstName;
+    @Column(name="last_name")
     private String lastName;
+    @Column(name="profile_pic")
+    private String profilePic;
+    @Column(name="username")
+    private String username;
+    @Column(name="professional_url")
+    private String professionalURL;
+    @Column(name="location")
+    private String location;
+    @Column(name="name_pronunciation")
+    private String namePronunciation;
 }
