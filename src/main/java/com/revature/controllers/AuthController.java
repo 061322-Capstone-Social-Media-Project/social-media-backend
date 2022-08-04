aa5c4a726e4ebaa1ff659112b47a4e91f9afac83
EFerri222 <eferri222@gmail.com>
2022-08-04 17:04:44 -0400

    Allow users to register

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -42,14 +42,19 @@ public class AuthController {
         return ResponseEntity.ok().build();
     }
 
-//    @PostMapping("/register")
-//    public ResponseEntity<User> register(@RequestBody RegisterRequest registerRequest) {
-//        User created = new User(0,
-//                registerRequest.getEmail(),
-//                registerRequest.getPassword(),
-//                registerRequest.getFirstName(),
-//                registerRequest.getLastName());
-//
-//        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(created));
-//    }
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest registerRequest) {
+        User created = new User(0,
+                registerRequest.getEmail(),
+                registerRequest.getPassword(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+        		registerRequest.getProfilePic(),
+				registerRequest.getUsername(),
+				registerRequest.getProfessionalURL(),
+				registerRequest.getLocation(),
+				registerRequest.getNamePronunciation());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(created));
+    }
 }
