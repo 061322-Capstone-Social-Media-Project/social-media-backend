8c01bbe4842192daf16ad2493a7550df7eb94bc9
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-08-05 11:11:18 -0400

    chore: replace controller annotations with component based cors fix

--- src/main/java/com/revature/SocialMediaApplication.java
+++ src/main/java/com/revature/SocialMediaApplication.java
@@ -2,6 +2,9 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SocialMediaApplication {
@@ -10,4 +13,15 @@ public class SocialMediaApplication {
 		SpringApplication.run(SocialMediaApplication.class, args);
 	}
 
+	@Component
+	public static class CorsFix implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedOrigins("http://localhost:4200", "http://ec2-3-138-189-139.us-east-2.compute.amazonaws.com:4200/login")
+					.allowedHeaders("*")
+					.exposedHeaders("Authorization")
+					.allowedMethods("GET", "OPTIONS", "PUT", "POST", "DELETE");
+		}
+	}
 }
