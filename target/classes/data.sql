6f16cb7653a3fb75c9d808281e789598f90a1980
EFerri222 <eferri222@gmail.com>
2022-08-05 16:02:34 -0400

    Prep for merge

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,20 +1,17 @@
-INSERT INTO users (id, email, password, first_name, last_name) VALUES (
-    1,
-    'testuser@gmail.com',
-    'password',
-    'Test',
-    'User'
-);
+insert into users
+(email, password, first_name, last_name, profile_pic, username, professional_url, location, name_pronunciation)
+values 
+('ftest@gmail.com', 'pass123', 'First', 'Test', 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-512.png', 'ftest', 'http://google.com/', 'Washington, D.C.', 'First Test');
 
-INSERT INTO posts (id, text, image_url, author_id) VALUES (
-    10000,
-    'The classic',
-    'https://i.imgur.com/fhgzVEt.jpeg',
-    1
-),
-(
-    10001,
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    '',
-    1
-); 
+-- INSERT INTO posts (id, text, image_url, author_id) VALUES (
+--     10000,
+--     'The classic',
+--     'https://i.imgur.com/fhgzVEt.jpeg',
+--     1
+-- ),
+-- (
+--     10001,
+--     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+--     '',
+--     1
+-- ); 
