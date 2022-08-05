18e1914657917ebec7a88b98c83dcbc773e1974c
Calvin Post <calvinhenry826@revature.net>
2022-08-05 15:54:14 -0400

    Changed FollowerController to pull from HttpSession. Added UserDTO

--- src/main/java/com/revature/controllers/FollowerController.java
+++ src/main/java/com/revature/controllers/FollowerController.java
@@ -1,6 +1,9 @@
 package com.revature.controllers;
 
+import java.util.HashSet;
 import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.revature.annotations.Authorized;
+import com.revature.dtos.UserDTO;
 import com.revature.keys.FollowerKey;
 import com.revature.models.User;
 import com.revature.services.FollowerService;
@@ -24,26 +27,44 @@ public class FollowerController {
 		this.fs = fs;
 	}
 
-	@Authorized
+//	@Authorized
 	@GetMapping("/following")
-	public ResponseEntity<Set<User>> getFollowing(@RequestBody User u) {
-		return ResponseEntity.ok(fs.getFollowingByFollower(u));
+	public ResponseEntity<Set<UserDTO>> getFollowing(HttpSession session) {
+		User currentUser = (User) session.getAttribute("user");
+		Set<UserDTO> fdto = new HashSet<>();
+		fs.getFollowingByFollower(currentUser).forEach(u -> {
+			UserDTO f = new UserDTO();
+			f.setId(u.getId());
+			f.setFirstName(u.getFirstName());
+			f.setLastName(u.getLastName());
+			fdto.add(f);
+		});
+		return ResponseEntity.ok(fdto);
 	}
 	
-	@Authorized
+//	@Authorized
 	@GetMapping("/followers")
-	public ResponseEntity<Set<User>> getFollowers(@RequestBody User u) {
-		return ResponseEntity.ok(fs.getFollowersByFollowing(u));
+	public ResponseEntity<Set<UserDTO>> getFollowers(HttpSession session) {
+		User currentUser = (User) session.getAttribute("user");
+		Set<UserDTO> fdto = new HashSet<>();
+		fs.getFollowersByFollowing(currentUser).forEach(u -> {
+			UserDTO f = new UserDTO();
+			f.setId(u.getId());
+			f.setFirstName(u.getFirstName());
+			f.setLastName(u.getLastName());
+			fdto.add(f);
+		});
+		return ResponseEntity.ok(fdto);
 	}
 	
-	@Authorized
+//	@Authorized
 	@PostMapping("/following")
 	public ResponseEntity<String> addFollowing(@RequestBody FollowerKey fk) {
 		fs.addFollowing(fk);
 		return ResponseEntity.ok("Followed");
 	}
 	
-	@Authorized
+//	@Authorized
 	@DeleteMapping("/following")
 	public ResponseEntity<String> removeFollowing(@RequestBody FollowerKey fk) {
 		fs.removeFollowing(fk);
