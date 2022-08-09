33595454ba969a871113454015e3375aae1b5ec3
EFerri222 <eferri222@gmail.com>
2022-08-09 11:45:23 -0400

    Update UserServiceTests

--- src/test/java/com/revature/services/UserServiceTests.java
+++ src/test/java/com/revature/services/UserServiceTests.java
@@ -3,6 +3,7 @@ package com.revature.services;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,14 +18,14 @@ class FollowerServiceTests {
 	@MockBean
 	private UserRepository ur;
 
-	@MockBean
-	private UserService us;
-
 	@Autowired
-	private UserService sut;
+	private UserService us;
 
 	@Test
 	void findByCredentialsNotExist() {
-		assertThrows(UserNotFoundException.class, () -> sut.findByCredentials("doesnt@exist.com", "nopass"));
+
+		Mockito.doThrow(IllegalArgumentException.class).when(ur).findByEmailAndPassword("doesnt@exist.com", "nopass");
+
+		assertThrows(UserNotFoundException.class, () -> us.findByCredentials("doesnt@exist.com", "nopass"));
 	}
 }
