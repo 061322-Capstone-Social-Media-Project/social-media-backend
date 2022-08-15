438b82eb47f7c4cb27c4a84aaabf0d33d8ea9354
EFerri222 <eferri222@gmail.com>
2022-08-15 12:01:40 -0400

    Fix code smell: remove unused import from UserRepository

--- src/main/java/com/revature/repositories/UserRepository.java
+++ src/main/java/com/revature/repositories/UserRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.revature.models.User;
 @Repository
