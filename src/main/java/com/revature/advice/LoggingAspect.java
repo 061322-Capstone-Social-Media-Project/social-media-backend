f084fcf8b1aee8491ab12d1db4d729d2746bb8b4
Calvin Post <calvinhenry826@revature.net>
2022-08-16 14:21:39 -0400

    Modified log methods

--- src/main/java/com/revature/advice/LoggingAspect.java
+++ src/main/java/com/revature/advice/LoggingAspect.java
@@ -18,28 +18,25 @@ public class LoggingAspect {
 	private static Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
 	@AfterThrowing(pointcut = "within(com.revature..*)", throwing = "e")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
-        log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
-            joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
-    }
-	
+	public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+		log.error("Exception in {}.{}() with cause = {}", joinPoint.getSignature().getDeclaringTypeName(),
+				joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+	}
+
 	@Around("within(com.revature..*)")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        
-            log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
-        
-        try {
-            Object result = joinPoint.proceed();
-            
-                log.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
-                    joinPoint.getSignature().getName(), result);
-            
-            return result;
-        } catch (IllegalArgumentException e) {
-            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
-                joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-            throw e;
-        }
-    }
+	public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+		log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+				joinPoint.getSignature().getName(),
+				joinPoint.getArgs().length > 0 ? Arrays.toString(joinPoint.getArgs()) : null);
+		try {
+			Object result = joinPoint.proceed();
+			log.info("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(),
+					joinPoint.getSignature().getName(), result);
+			return result;
+		} catch (IllegalArgumentException e) {
+			log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()),
+					joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+			throw e;
+		}
+	}
 }
